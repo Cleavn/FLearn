c14ca2137654e53175e79223118209d22d6bd03d
Cleavn <vdnguyen91@gmail.com>
2018-04-27 18:53:38 +0200

    Initial Commit

--- app/src/main/java/cleavn/flearn/CategoryAdapter.java
+++ app/src/main/java/cleavn/flearn/CategoryAdapter.java
@@ -3,17 +3,34 @@ package cleavn.flearn;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+class CategoryAdapter extends ArrayAdapter<String> {
 
-public class CategoryAdapter extends ArrayAdapter<Category>{
-
-    public CategoryAdapter(Context context, ArrayList<Category> category) {
-        super(context,0 ,category);
+    public CategoryAdapter(@NonNull Context context, String[] categoryName, String[] categoryDescr) {
+        super(context, R.layout.category_item , categoryName);
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View categoryView = layoutInflater.inflate(R.layout.category_item, parent, false);
+
+        String categoryItem = getItem(position);
+        String categoryItemDescr = getItem(position);
+        View categoryColor = (View) categoryView.findViewById(R.id.colorImageView);
+        TextView categoryName = (TextView) categoryView.findViewById(R.id.categoryNameTextView);
+        TextView categoryDescr = (TextView) categoryView.findViewById(R.id.categoryDescrTextView);
+
+        categoryColor.setBackgroundColor(0xFF00FF00);
+        categoryName.setText(categoryItem);
+        categoryDescr.setText(categoryItemDescr);
+
+        return categoryView;
+    }
 }
--- app/src/main/java/cleavn/flearn/MainActivity.java
+++ app/src/main/java/cleavn/flearn/MainActivity.java
@@ -3,29 +3,42 @@ package cleavn.flearn;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     // Create list of dummycategories
+
     String[] dummycategories = {"english", "math", "whatever"};
     String[] dummycatdescr = {"business english vocabluary", "formular", "whatever"};
+
+    Category cat1 = new Category(dummycategories[1],dummycatdescr[1], Category.CategoryColor.BLUE);
+
+    Category[] categories = new Category[] {cat1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListAdapter categoryAdapter = new CategoryAdapter(this, dummycategories, dummycatdescr);
+        ListView listView = (ListView) findViewById(R.id.categoryListView);
+        listView.setAdapter(categoryAdapter);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_launcher);
 
-        fillListView();
-    }
-
-    private void fillListView() {
 
     }
+
 }
